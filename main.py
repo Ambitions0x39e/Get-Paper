3f570d799d62b34a89c8edd06f525ccd5b263cc7
Ambitions0x39e <gwuchanga@gmail.com>
2024-03-27 18:01:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,14 @@
-import sys
-from PySide6 import QtCore, QtWidgets
+import sys, os, time
 from src.info import subjects
-from src.write_pdf import download_pdf
-from Cocoa import NSObject
-from AppKit import NSUserNotificationCenter, NSUserNotification
-from src.notifications import NotificationDelegate, send_notification
-# Example Link: https://cie.fraft.cn/obj/Fetch/redir/9709_m20_ms_22.pdf
+from PySide6 import QtCore, QtWidgets
+from src.central import paper_download
 
-def main(Paper_Code, Year, Season, Paper_Number, Qp_Ms):
-    # FULL year if user only inputs the last two digits of the year number
-    if len(Paper_Code) != 4: return "Wrong Paper Code"
-    if len(Year) == 2: Year = '20' + Year
-    seasons = ''
-    seasons = 'm' if Season.lower() in ['march', 'mar', 'feb', 'february', 'spring'] \
-        else 's' if Season.lower() in ['may', 'june', 'jun', 'summer'] \
-        else 'w' if Season.lower() in ['oct', 'october', 'nov', 'november', 'winter'] \
-        else ''
-    if seasons == '':
-        return 'Wrong Seasons'
-    qp_name = f'{Paper_Code}_{seasons}{Year[2:]}_qp_{Paper_Number}'
-    ms_name = f'{Paper_Code}_{seasons}{Year[2:]}_ms_{Paper_Number}'
-    src_qp= 'https://cie.fraft.cn/obj/Fetch/redir/'+f'{qp_name}.pdf'
-    src_ms= 'https://cie.fraft.cn/obj/Fetch/redir/'+f'{ms_name}.pdf'
-    
-    if Qp_Ms == '':
-        try: 
-            download_pdf(src_qp, qp_name)
-            download_pdf(src_ms, ms_name)
-        except ConnectionResetError:
-            return "Wrong Network! Please check your network / proxies"
-    elif Qp_Ms.lower() == 'qp':
-        try:
-            download_pdf(src_qp, qp_name)
-        except ConnectionResetError:
-            return "Wrong Network! Please check your network / proxies"
-    elif Qp_Ms.lower() == 'ms': 
-        try:
-            download_pdf(src_ms, ms_name)
-        except ConnectionResetError:
-            return "Wrong Network! Please check your network / proxies"
-    else:
-        return "Wrong QP / MS input!"
-    
-    return 'Success'
-    
+# Example Link: https://cie.fraft.cn/obj/Fetch/redir/9709_m20_ms_22.pdf
+'''
+'''
+
+  
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -59,6 +24,8 @@ class MyWidget(QtWidgets.QWidget):
         label_3 = QtWidgets.QLabel("Seasons")
         label_4 = QtWidgets.QLabel("Paper Number")
         label_5 = QtWidgets.QLabel("QP / MS")
+        self.empty_line = QtWidgets.QLabel(" ")
+        self.status_text = QtWidgets.QLabel(" ", alignment=QtCore.Qt.AlignHCenter)
 
         self.line_edit_1 = QtWidgets.QLineEdit()
         self.line_edit_2 = QtWidgets.QLineEdit()
@@ -68,6 +35,7 @@ class MyWidget(QtWidgets.QWidget):
 
         self.button = QtWidgets.QPushButton("↓ Download Paper ↓")
         self.exit_button = QtWidgets.QPushButton("Quit App")
+        self.clear_button = QtWidgets.QPushButton("Clear all Papers")
         
 
         form_layout = QtWidgets.QFormLayout()
@@ -79,8 +47,12 @@ class MyWidget(QtWidgets.QWidget):
 
         main_layout = QtWidgets.QVBoxLayout()
         main_layout.addLayout(form_layout)
+        main_layout.addWidget(self.empty_line)
+        main_layout.addWidget(self.status_text)
+        main_layout.addWidget(self.empty_line)
         main_layout.addWidget(self.button)
         main_layout.addWidget(self.exit_button)
+        main_layout.addWidget(self.clear_button)
 
         self.setLayout(main_layout)
 
@@ -100,17 +72,24 @@ class MyWidget(QtWidgets.QWidget):
             paper_number = self.line_edit_4.text()
             qp_ms = self.line_edit_5.text()
             
-            result = main(paper_code, year, season, paper_number, qp_ms)
+            result = paper_download(paper_code, year, season, paper_number, qp_ms)
             
-            print(result)
-            # send_notification('Get Paper', '', result)
+            # Suppose the Py version is default python from Apple. 
+            # send_notifications('Get Paper', '', result)
+            self.status_text.setText(result)
+            time.sleep(5)
+            self.status_text.setText(" ")
+            
             # 在状态标签中显示获取论文的状态信息
+        def clear_cache():
+            os.system("find ~/Downloads/Past_Papers -mindepth 1 -delete >/dev/null 2>&1")
+            self.status_text.setText("All Papers cleared!")
 
         self.button.clicked.connect(Get_Paper)
         self.exit_button.clicked.connect(exit)
+        self.clear_button.clicked.connect(clear_cache)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MyWidget()
     window.show()
