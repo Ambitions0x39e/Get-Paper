cb6e17d2418952c9df009fa17fd893f0aa076f9d
Ambitions0x39e <gwuchanga@gmail.com>
2024-03-27 18:01:41 +0800

    Update notifications.py

--- src/notifications.py
+++ src/notifications.py
@@ -1,16 +1,3 @@
-class NotificationDelegate(NSObject): # Class for Notification Present
-    def userNotificationCenter_didActivateNotification_(self, center, notification):
-        # Handle notification activation
-        pass
-    
-def send_notification(title, subtitle, message):
-    delegate = NotificationDelegate.alloc().init()
-    center = NSUserNotificationCenter.defaultUserNotificationCenter()
-    center.setDelegate_(delegate)
+from functools import partial
+from mac_notifications import client
 
-    notification = NSUserNotification.alloc().init()
-    notification.setTitle_(title)
-    notification.setSubtitle_(subtitle)
-    notification.setInformativeText_(message)
-
-    center.deliverNotification_(notification)
